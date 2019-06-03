61536f276e6a9fbfe4f76966ed285f7384d3aa9d
bradtaylor <bradt@broadinstitute.org>
2019-06-02 23:45:25 -0400

    Add notebook conversion via nbconvert
    Import the nbconvert library and programmatically perform the request json -> html conversion
    
    I tested this manually using a few notebooks, including tables and images.
    output was visually equivalent in the browser to nbconvert on command line, and to Calhoun

--- main.py
+++ main.py
@@ -1,6 +1,21 @@
 from sanic import Sanic
 from sanic import response
+from nbconvert import HTMLExporter
+from nbformat.v4 import to_notebook
 
+
+# Define our main conversion function as a coroutine so it can be awaited
+async def perform_conversion(notebook_json):
+    # Get the notebook json into a NotebookNode object that nbconvert can use
+    nb = to_notebook(notebook_json)
+
+    # set up a default nbconvert HTML exporter and run the conversion
+    html_exporter = HTMLExporter()
+    (nb_html, resources_dict) = html_exporter.from_notebook_node(nb)
+    return nb_html
+
+
+# Webservice routing
 app = Sanic('douglass')
 
 
@@ -9,9 +24,9 @@ async def status_response(request):
     return response.text('OK')
 
 
-@app.route('/api/convert')
+@app.route('/api/convert', methods=['POST'])
 async def convert(request):
-    return response.html('<h1>HelloWorld</h1>')
+    return response.html(await perform_conversion(request.json))
 
 
 if __name__ == '__main__':
